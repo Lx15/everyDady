f9c84bfb394dadd4f23e8ebeb1460955ad29bded
Lx <123@qq.com>
2016-09-25 23:41:37 +0800

    9/25 ife task6

---
+++ Java/Java01/src/helloworld/Quadrangle.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+/**
+ * Created by pc on 2016/9/23.
+ */
+public class Quadrangle {
+    public static  void  draw(Quadrangle a){
+        System.out.println("zzz");
+    }
+}
+
